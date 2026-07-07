3d452793d766836423cd86c2f77d3fec28dda096
Ekshin683 <ekshindeep@gmail.com>
2026-07-07 12:43:42 +0530

    Update

--- WEEK-1/MODULE-4/Exercises/src/test/java/com/exercises/AssertionsTest.java
+++ WEEK-1/MODULE-4/Exercises/src/test/java/com/exercises/AssertionsTest.java
@@ -1,7 +1,15 @@
 package com.exercises;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class AssertionsTest {
 
