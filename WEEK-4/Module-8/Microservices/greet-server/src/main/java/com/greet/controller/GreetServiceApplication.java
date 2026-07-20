a6451b41e27718b12db5feba734e4819ef3db1dc
Ekshin683 <ekshindeep@gmail.com>
2026-07-20 13:24:28 +0530

    Update

---
+++ WEEK-4/Module-8/Microservices/greet-server/src/main/java/com/greet/controller/GreetServiceApplication.java
@@ -0,0 +1,16 @@
+public class GreetServiceApplication {
+    
+}
+package com.cts.greet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GreetServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GreetServiceApplication.class, args);
+    }
+}
